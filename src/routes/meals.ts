30e12e96ee8cf0b642c8ae61fd69b707e6685b89
Carlos Alexandre <alexfilho062005@gmail.com>
2024-10-09 16:21:25 -0300

    list all meals

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -69,4 +69,27 @@ export async function mealsRoutes(app: FastifyInstance) {
 
     return reply.status(200).send({ message: 'Refeição deletada com sucesso' })
   })
+
+  app.get('/user/:userId', async (request, reply) => {
+    const paramsSchema = z.object({
+      userId: z.string(),
+    })
+
+    const { userId } = paramsSchema.parse(request.params)
+
+    try {
+      const meals = await knex('meals').select('*').where('user_id', userId)
+
+      if (meals.length === 0) {
+        return reply
+          .status(404)
+          .send({ message: 'Nenhuma refeição encontrada para este usuário.' })
+      }
+
+      return reply.status(200).send(meals)
+    } catch (error) {
+      console.error(error)
+      return reply.status(500).send({ message: 'Erro ao buscar refeições.' })
+    }
+  })
 }
