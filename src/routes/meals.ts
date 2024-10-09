cd0937e68a2e6af6f602adb503a670b4d983c9d1
Carlos Alexandre <alexfilho062005@gmail.com>
2024-10-09 17:27:51 -0300

    feat: metrics and authorization

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -34,24 +34,27 @@ export async function mealsRoutes(app: FastifyInstance) {
       description: z.string(),
       mealDatetime: z.string(),
       isDiet: z.boolean(),
+      userId: z.string(),
     })
 
     const { id } = request.params as { id: string }
+    const { name, description, mealDatetime, isDiet, userId } =
+      updateMealSchema.parse(request.body)
 
-    const { name, description, mealDatetime, isDiet } = updateMealSchema.parse(
-      request.body,
-    )
+    const meal = await knex('meals').where({ id, user_id: userId }).first()
 
-    const result = await knex('meals').where({ id }).update({
+    if (!meal) {
+      return reply
+        .status(404)
+        .send({ message: 'Refeição não encontrada ou não pertence ao usuário' })
+    }
+
+    await knex('meals').where({ id }).update({
       name,
       description,
       mealDatetime,
       isDiet,
     })
-
-    if (result === 0) {
-      return reply.status(404).send({ message: 'Refeição não encontrada' })
-    }
 
     return reply
       .status(200)
@@ -60,12 +63,17 @@ export async function mealsRoutes(app: FastifyInstance) {
 
   app.delete('/:id', async (request, reply) => {
     const { id } = request.params as { id: string }
+    const { userId } = request.query as { userId: string }
 
-    const result = await knex('meals').where({ id }).del()
+    const meal = await knex('meals').where({ id, user_id: userId }).first()
 
-    if (result === 0) {
-      return reply.status(404).send({ message: 'Refeição não encontrada' })
+    if (!meal) {
+      return reply
+        .status(404)
+        .send({ message: 'Refeição não encontrada ou não pertence ao usuário' })
     }
+
+    await knex('meals').where({ id }).del()
 
     return reply.status(200).send({ message: 'Refeição deletada com sucesso' })
   })
@@ -113,6 +121,56 @@ export async function mealsRoutes(app: FastifyInstance) {
     } catch (error) {
       console.error(error)
       return reply.status(500).send({ message: 'Erro ao buscar a refeição.' })
+    }
+  })
+
+  app.get('/user/:userId/metrics', async (request, reply) => {
+    const paramsSchema = z.object({
+      userId: z.string(),
+    })
+
+    const { userId } = paramsSchema.parse(request.params)
+
+    try {
+      const totalMeals = await knex('meals')
+        .where('user_id', userId)
+        .count('* as count')
+        .first()
+      const totalDietMeals = await knex('meals')
+        .where({ user_id: userId, isDiet: true })
+        .count('* as count')
+        .first()
+      const totalNonDietMeals = await knex('meals')
+        .where({ user_id: userId, isDiet: false })
+        .count('* as count')
+        .first()
+
+      const meals = await knex('meals')
+        .where('user_id', userId)
+        .orderBy('mealDatetime', 'asc')
+      let bestDietStreak = 0
+      let currentStreak = 0
+
+      meals.forEach((meal) => {
+        if (meal.isDiet) {
+          currentStreak++
+          if (currentStreak > bestDietStreak) {
+            bestDietStreak = currentStreak
+          }
+        } else {
+          currentStreak = 0
+        }
+      })
+
+      return reply.status(200).send({
+        totalMeals: totalMeals?.count ?? 0,
+        totalDietMeals: totalDietMeals?.count ?? 0,
+        totalNonDietMeals: totalNonDietMeals?.count ?? 0,
+        bestDietStreak,
+      })
+    } catch (error) {
+      console.error(error)
+      return reply.status(500).send({ message: 'Erro ao buscar métricas.' })
     }
   })
 }
