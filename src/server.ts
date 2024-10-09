933e927127223920f34e61a43dcf6d9370d88b94
Carlos Alexandre <alexfilho062005@gmail.com>
2024-10-09 12:54:07 -0300

    feat: migration meals

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import fastify from 'fastify'
 import { env } from './env'
 import { usersRoutes } from './routes/users'
+import { mealsRoutes } from './routes/meals'
 
 const app = fastify()
 
 app.register(usersRoutes)
+app.register(mealsRoutes)
 
 app
   .listen({
