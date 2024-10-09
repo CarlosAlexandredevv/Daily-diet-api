4cc7eae48ddddcee20696a31f88fd2cd0eb64e73
Carlos Alexandre <alexfilho062005@gmail.com>
2024-10-09 14:31:26 -0300

    refactor: migration, routes ...

--- db/migrations/20241009172459_create-user.ts
+++ db/migrations/20241009172459_create-user.ts
@@ -4,10 +4,6 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('users', (table) => {
     table.uuid('id').primary()
     table.text('name').notNullable()
-    table.integer('totalMeals').notNullable()
-    table.integer('totalMealsInDiet').notNullable()
-    table.integer('totalMealsOutDiet').notNullable()
-    table.integer('streakMeals').notNullable()
   })
 }
 
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,28 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { z } from 'zod'
+import { randomUUID } from 'crypto'
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.get('/users', async () => {
-    const users = await knex('users').select('*')
+  app.post('/', async (request, reply) => {
+    const createUserSchema = z.object({
+      name: z.string(),
+      totalMeals: z.number(),
+      totalMealsInDiet: z.number(),
+      totalMealsOutDiet: z.number(),
+      streakMeals: z.number(),
+    })
 
+    const { name } = createUserSchema.parse(request.body)
+
+    await knex('users').insert({
+      id: randomUUID(),
+      name,
+    })
+    return reply.status(201).send()
+  })
+  app.get('/', async () => {
+    const users = await knex('users').select('*')
     return users
   })
 }
